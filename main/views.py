e9b5b325dc5a0c257809a9ff0b5415baebc8d831
Sima3443 <toppochka@gmail.com>
2023-12-27 22:44:07 +0200

    demo home page

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 # Create your views here.
 def index(request) -> HttpResponse:
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'sd':1})
 
 def about(request) -> HttpResponse:
     return HttpResponse('about page')

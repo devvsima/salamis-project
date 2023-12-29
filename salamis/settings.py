ce476f2dc0bb19467cd2fe405fa7310a8e6426ac
Sima3443 <toppochka@gmail.com>
2023-12-29 20:06:03 +0200

    add debug-toolbar

--- salamis/settings.py
+++ salamis/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-        
+    
+    'debug_toolbar',
+
     'main',
     'goods',
-
-
-
+]
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'salamis.urls'

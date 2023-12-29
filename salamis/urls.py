e15beead621a152e7cc0affd07294d83e2537d76
Sima3443 <toppochka@gmail.com>
2023-12-29 13:42:50 +0200

    add beta-catalog

--- salamis/urls.py
+++ salamis/urls.py
@@ -15,16 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main import views
+from django.urls import path, include
+
 
 urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
 
-    # main
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
 
 ]

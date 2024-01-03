dda46900b2982a15102ec042e254982a203fbd79
Sima <toppochka@gmail.com>
2024-01-03 14:26:29 +0200

    add normal page

--- goods/views.py
+++ goods/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from goods.models import Categories, Products
 
-def catalog(request, category_slug='all', page=1):
+def catalog(request, category_slug='all'):
+
+    page = request.GET.get('page', 1)
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = Products.objects.filter(category__slug=category_slug)
     
-    paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    paginator = Paginator(goods, 4)
+    current_page = paginator.page(int(page))
     context = {'products': current_page,
                'slug_url': category_slug}
 

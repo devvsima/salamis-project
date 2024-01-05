0acf15f5a87e5c672310ac3d6484be572f648cf9
Sima <toppochka@gmail.com>
2024-01-05 15:47:54 +0200

    work sorted

--- goods/views.py
+++ goods/views.py
@@ -2,25 +2,35 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from goods.models import Categories, Products
 
-def catalog(request, category_slug='all'):
+def catalog(request, category_slug="all"):
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
 
-    page = request.GET.get('page', 1)
-    if category_slug == 'all':
-        goods = Products.objects.all()
-    else:
-        goods = Products.objects.filter(category__slug=category_slug)
-    
+
+    if category_slug == "all": goods = Products.objects.all()
+    else: goods = Products.objects.filter(category__slug=category_slug)
+
+    if on_sale: goods = goods.filter(discount__gt=0)
+    if order_by and order_by != 'default': goods = goods.order_by(order_by)
+
+
     paginator = Paginator(goods, 4)
     current_page = paginator.page(int(page))
-    context = {'products': current_page,
-               'slug_url': category_slug}
 
-    return render(request,'goods/catalog.html', context)
+    context = {
+        "products": current_page,
+        "slug_url": category_slug,
+    }
+
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request, product_slug=False, product_id=False):
-    if product_slug: product = Products.objects.get(slug=product_slug)
-    elif product_id: product = Products.objects.get(id=product_id)
-    
-    context = {'product': product}
-    return render(request,'goods/product.html', context)
+    if product_slug:
+        product = Products.objects.get(slug=product_slug)
+    elif product_id:
+        product = Products.objects.get(id=product_id)
+
+    context = {"product": product}
+    return render(request, "goods/product.html", context)

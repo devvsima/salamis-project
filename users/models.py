05023cbf0f54dc5da4b0064557dba2707133cb3b
Sima3443 <toppochka@gmail.com>
2024-01-08 18:40:45 +0200

    refresh db

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Фото профілю')
+    phone_number = models.CharField(max_length=12, blank=True, null=True, verbose_name='Номер телефону')
+
+    class Meta:
+        db_table = "user"
+        verbose_name = "користувач"
+        verbose_name_plural = "Користувачі"
+
+    def __str__(self) -> str:
+        return self.username
+          

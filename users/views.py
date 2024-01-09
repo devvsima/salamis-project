3b0f522b01670b4f9e07fa93a883848a4d7e9a72
Sima3443 <toppochka@gmail.com>
2024-01-09 17:33:11 +0200

    work login

--- users/views.py
+++ users/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .forms import UserLoginForm
 
 def registration(request):
     context = {
@@ -8,8 +13,22 @@ def registration(request):
     return render(request, 'users/registration.html', context)
  
 def login(request):
+    
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:   
+        form = UserLoginForm()
+
     context = {
         'title': "Login",
+        'form': form
     }
 
     return render(request, 'users/login.html', context)

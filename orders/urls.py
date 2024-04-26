e6e6da69bf52036de1e4940f57900a7cb49ab909
Sima3443 <toppochka@gmail.com>
2024-04-26 04:55:51 +0300

    order done imetation

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,5 @@ app_name = 'orders'
 
 urlpatterns = [
     path('create-order/', views.create_order, name='create_order'),
+    path('done-order/', views.done_order, name='done_order'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def create_order(request):
     return render(request, 'orders/create_order.html')
+
+def done_order(request):
+    return render(request, 'orders/done_order.html')
+    

9ebb5798f3b91f827c49bf5052e24bc8434fd3f9
Sima3443 <toppochka@gmail.com>
2024-04-26 05:01:10 +0300

    work + in cart

--- carts/views.py
+++ carts/views.py
@@ -18,6 +18,7 @@ def cart_add(request, product_slug):
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
 
+
     return redirect(request.META["HTTP_REFERER"])
 
 def cart_change(request, product_slug):

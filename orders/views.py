6596b1fad8079ad315c57cf30844ea11b3c75d3d
Sima3443 <toppochka@gmail.com>
2024-05-08 12:49:32 +0300

    save

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from carts.models import Cart
+from users.models import User
 # Create your views here.
 def create_order(request):
     return render(request, 'orders/create_order.html')
 
-def done_order(request):
+def done_order(request, user_id):
+    user_to_delete = User.objects.get(id=user_id)
+    carts_to_delete = Cart.objects.filter(user=user_to_delete)
+    carts_to_delete.delete()
     return render(request, 'orders/done_order.html')
     

5bf2310d90ecd996e51379d56e72f396067f1f38
Sima3443 <toppochka@gmail.com>
2024-08-09 16:17:27 +0300

    save

--- carts/views.py
+++ carts/views.py
@@ -40,8 +40,8 @@ def cart_change(request, product_slug):
 
 
 
-def cart_remove(request, cart_id):
-    cart = Cart.objects.get(id=cart_id)
+def cart_remove(request, product_slug):
+    cart = Cart.objects.get(slug=product_slug)
     cart.delete()
 
     return redirect(request.META["HTTP_REFERER"])
